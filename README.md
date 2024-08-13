149a9c980d37b9f5ec0f8469185a5ca414898f12
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-13 11:09:02 -0700

    initial commits

---
+++ README.md
@@ -0,0 +1,30 @@
+# sol-actor
+
+`Actor` is a contract which provides an **actor** with properties such as `name`, `summay`, `type`, etc, for the Solidity programming language.
+
+You would probably use `Actor` to for something like a social-media networking profile.
+
+## Installation
+
+To install using `forge` do the following:
+
+```
+forge install https://github.com/reiver/sol-actor
+```
+
+And then append the following to your `remappings.txt` file:
+
+```
+@reiver/actor/=lib/sol-actor/lib/
+```
+
+## Import
+
+To import use `import` code like the follownig:
+```
+import "@reiver/actor/actor.sol";
+```
+
+## Author
+
+Package **actor** was written by [Charles Iliya Krempeaux](http://reiver.link)
---
+++ lib/actor.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.13;
+
+import "@reiver/delegatable/delegatable.sol";
+import "@reiver/identifiable/primarilyidentifiable.sol"
+
+contract Actor is Delegatable, PrimarilyIdentifiable {
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// STORAGE                                                                                        //
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	string private _name;
+	string private _summary;
+	string private _type;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTOR, FALLBACKS                                                                         //
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	constructor(uint actorid) PrimarilyIdentifiable(actorid) {
+		// nothing here
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// PUBLIC FUNCTIONS                                                                               //
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	function setActorName(string value) public onlyOwnerOrDelegate {
+		_name = value;
+	}
+
+	function setActorSummary(string value) public onlyOwnerOrDelegate {
+		_summary = value;
+	}
+
+	function setActorType(string value) public onlyOwnerOrDelegate {
+		_type = value;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+        // EXTERNAL VIEWS                                                                                 //
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	function actorName() public view returns (string) {
+		return _name;
+	}
+
+	function actorSummary() public view returns (string) {
+		return _summary;
+	}
+
+	function actorType() public view returns (string) {
+		return _type;
+	}
+}
